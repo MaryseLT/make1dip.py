7ec62bd4e37de23cbc901d9b17c0569916fb9b30
Maryse <35494636+MaryseLT@users.noreply.github.com>
2021-10-18 16:55:00 -0400

    Cleaned up code

--- make1dip.py
+++ make1dip.py
@@ -1,105 +1,72 @@
-# https://pythonprogramming.altervista.org/join-all-mp4-with-python-and-ffmpeg/
-#https://python-forum.io/thread-7009.html
-### https://kkroening.github.io/ffmpeg-python/
-
 import os
 from os.path import join
 import subprocess
 import pathlib
-# pathlib.Path(_file_).parent.resolve()
-# pathlib.Path().resolve()
+from bhl_born_digital_utils.config import get_config_setting
 
-# def clean_count(directory):
 def clean_count(directory):
     """ Identify the MP4s and write to TXT """
 
     def topDir(listpath):
-        total_size = 0 # individual sizes
-        count = 0
+        count = 0 # Old, but it keeps the script from breaking. Fix later =/
 
         """ Run CMD from TXT """
         readlist = open(listpath,'r')
         print(readlist.read())
         print("-----------")
 
+        ffmpeg_path = get_config_setting("ffmpeg", default="ffmpeg") # < Critical, need path to FFMPEG on your machine
 
-        # for x in media:
-        #     count +=1
-
-        """ my attmempt below """
-
-
-        # cmd = [
-        #     ffmpeg_path,
-        #     '-f', 'concat',
-        #     '-safe', '0',
-        #     '-i', tmp_list,
-        #     '-c', 'copy',
-        #     out_filepath
-        # ]
-        #exit_code = subprocess.call(cmd)
-
-        # cmd = ["ffmpeg", "-f", "concat", "-i", "%s" % vid_list, "-c", "copy", "%s" % out_file]
-        ffmpeg_path = "PATH|TO|FFMPEG\"
         cmd = [
             ffmpeg_path,
             '-f', 'concat',
             '-safe', '0',
-            '-i', "%s" % vid_list,
-            "-c", "copy",
-            "%s" % out_file
+            '-i', abs_listpath,
+            '-c', 'copy',
+            out_file
         ]
-        exit_code = subprocess.run(cmd)
-        # os.remove(vid_list)
-        results = [barcode,exit_code]
+        exit_code = subprocess.call(cmd)
 
+        # deleting temporary input text file
+        #os.remove(abs_listpath) # Not in the right spot, deletes before execution is finished
         
         return count
-        #return results
+        
     """ END OF topDir """
 
-
     """ clean_count() ACTIONS """
-
-    """ Don't alter below """
+    for paths in os.listdir(directory):
+            media = []
+            listpath = join(directory, 'list.txt')
+            barcode = join(directory, paths) #Barcode folder paths
+            filePath = barcode.lower()
+    
     with open(listpath,'a') as f:
         for paths in os.listdir(directory):
             media = []
-            barcode = join(directory, paths) #Barcode folder paths
-            # filePath = barcode.lower()
-            filePath = barcode
+            barcode = join(directory, paths)
+            filePath = barcode # Same thing, but not risking it right now =]
             if filePath.endswith('.mp4'):
-                f.write(filePath +'\n')
+                # Doesn't order properly if -10 or higher, ATM fix that manually
+                f.write("file '"+filePath +"'\n")
             else:
                 pass
-        """ Don't alter above"""
-        # print(filePath)
-    # print("Loading list.txt file")
-    # time.sleep(10)
 
-    topDir(listpath) #KEEP!!!
-
-
-
+    topDir(listpath)
     return paths
     """ END OF clean_count """
 
-""" GLOBAL VARS """
-# path = input("Enter the Directory: ")
-
-directory = 'PATH\TO\COLL'
+coll_input = input("Enter Package FOLDER Path: ")
+directory = os.path.normpath(coll_input.strip('"\' '))
 d = os.path.split(directory)
-dname = d[-1] #out_file name
-out_file = dname+".mp4"
+dname = d[-1] #package name
+print(dname)
+outmp4 = dname+".mp4" #out file name
+out_file = join(directory,outmp4) #out file path
 
 vid_list = 'list.txt'
 listpath = join(directory, vid_list)
+abs_listpath = os.path.abspath(listpath) # FFMPEG WILL NOT WORK WITHOUT THIS!!!
 
-# print(dname)
-# path = r'WINDOWS\PATH\'
-# coll_input = input("Enter Collection Number: ")
-# collnum = str(coll_input)
-# coll = "R_"+collnum
-#directory = join(path,coll)
 
 clean_count(directory)
